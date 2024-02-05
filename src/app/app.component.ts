b5534703a15b7411e9e07536b2aeb605275bb285
Bertrand-Theo <145748421+Bertrand-Theo@users.noreply.github.com>
2024-02-05 17:36:39 +0100

    Commit un probléme regler mais le binding ne fonctionne toujours pas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {AppModule} from "./app.module";
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [
+    AppModule
+  ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here, permet le databinding et de faire en sorte que les components ne soit pas en standalone
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
-  declarations: [
-    AppComponent,
+  declarations: [ // Les component de l'app
     HeroesComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppComponent
+  imports: [ // Les packages
+    CommonModule,
+    FormsModule
   ],
-  providers: [],
-  exports: [HeroesComponent],
-  bootstrap: [AppComponent]
+  providers: [], //Les services de provider utiliser
+  exports: [ //Le component pouvant être utiliser et afficher par d'autre Ngmodules
+    HeroesComponent,
+  ],
+  bootstrap: [AppComponent] //Le component de lancement de l'app
 })
 export class AppModule { }
